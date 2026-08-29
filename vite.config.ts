8756d3f91b8ea76279d3c65957a1fc87c8714e55
anwar101-coder <anwaradem486@gmail.com>
2026-08-29 11:35:41 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   base: "/rediet-assefa-hotel/",
   tanstackStart: {
-    spa: {
-      enabled: true,
+    server: { entry: "server" },
+    prerender: {
+      enabled: false,
     },
   },
 });
